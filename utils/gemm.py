42c7550d6c9a06aef9f8d95d879f88fd032afffb
mathadoor <matharooh2@gmail.com>
2024-04-27 08:41:34 -0400

    Code reorganized to accommodate MLFlow based experiments

--- utils/gemm.py
+++ utils/gemm.py
@@ -1,3 +1,4 @@
+import mlflow
 import torch
 from torch import float32, int8, int16, int32
 
@@ -8,8 +9,6 @@ alpha_q, beta_q = -2 ** (bits - 1), 2 ** (bits - 1) - 1
 def quantization(x, s, z):
     x_q = torch.round(1 / s * x + z)
     x_q = torch.clamp(x_q, alpha_q, beta_q)
-    # x_q = np.round(1 / s * x + z, decimals=0)
-    # x_q = np.clip(x_q, a_min=alpha_q, a_max=beta_q)
 
     return x_q
 
@@ -50,8 +49,8 @@ def quantization_matrix_multiplication_int8(A_q, B_q, s_A, z_A, s_B, z_B, s_Y, z
     # Y_q_simulated is FP32
     Y_q_simulated = torch.zeros(A_q.shape[0], B_q.shape[1], dtype=int32)
     # outer produce
-    for k in range(p):
-        Y_q_simulated += torch.einsum("i,j->ij", (A_q[:, k].to(int) - z_A), (B_q[k, :].to(int) - z_B))
+
+    Y_q_simulated += torch.einsum("ij,jk->jk", (A_q.to(int) - z_A), (B_q.to(int) - z_B))
 
     Y_q_simulated = s_A * s_B * Y_q_simulated / s_Y + z_Y
 
@@ -79,74 +78,87 @@ def get_range(x, coverage=0.99):
     return x_min, x_max
 
 
-def main():
+def experiment(n=100, alpha_a=-1, beta_a=1, alpha_b=-1, beta_b=1, cover=0.99, random=0, dist_type="uniform"):
     # Set random seed for reproducibility
-    random_seed = 0
-    torch.random.manual_seed(random_seed)
+    torch.random.manual_seed(random)
+    params = dict()
 
-    # Random matrices
+    # Generate Matrices
+    A, B = generate_matrices(n, alpha_a, beta_a, alpha_b, beta_b, dist_type="uniform")
+    Y = torch.matmul(A, B)
 
-    m = 100
-    p = 100
-    n = 100
+    # Find the range of matrices
+    a_min, a_max = get_range(A, coverage=cover)
+    b_min, b_max = get_range(B, coverage=cover)
+    y_min, y_max = get_range(Y, coverage=cover)
+    params["a_min"] = a_min.item()
+    params["a_max"] = a_max.item()
+    params["b_min"] = b_min.item()
+    params["b_max"] = b_max.item()
+    params["y_min"] = y_min.item()
+    params["y_max"] = y_max.item()
 
-    # X
-    alpha_X = -1
-    beta_X = 1
-    s_X, z_X = generate_quantization_int8_constants(alpha=30 * alpha_X, beta=30 * beta_X)
-    X = torch.randn(m, p) * (beta_X - alpha_X) + alpha_X
-    X_q = quantization_int8(x=X, s=s_X, z=z_X)
-    X_q_dq = dequantization(x_q=X_q, s=s_X, z=z_X)
+    # Compute Quantization Constants
+    s_A, z_A = generate_quantization_int8_constants(alpha=a_min, beta=a_max)
+    s_B, z_B = generate_quantization_int8_constants(alpha=b_min, beta=b_max)
+    s_Y, z_Y = generate_quantization_int8_constants(alpha=y_min, beta=y_max)
+    params["s_A"] = s_A
+    params["z_A"] = z_A
+    params["s_B"] = s_B
+    params["z_B"] = z_B
+    params["s_Y"] = s_Y
+    params["z_Y"] = z_Y
 
-    # W
-    alpha_W = -1
-    beta_W = 1
-    s_W, z_W = generate_quantization_int8_constants(alpha=30 * alpha_W, beta=30 * beta_W)
-    W = torch.randn(p, n) * (beta_W - alpha_W) + alpha_W
-    W_q = quantization_int8(x=W, s=s_W, z=z_W)
-    W_q_dq = dequantization(x_q=W_q, s=s_W, z=z_W)
+    # Compute Quantized Matrix
+    A_q = quantization_int8(A, s_A, z_A)
+    B_q = quantization_int8(B, s_B, z_B)
+    Y_q = quantization_matrix_multiplication_int8(A_q, B_q, s_A, z_A, s_B, z_B, s_Y, z_Y)
+    Y_q_expected = quantization_int8(Y, s_Y, z_Y)
 
-    # Y
-    alpha_Y = -1
-    beta_Y = 1
-    s_Y, z_Y = generate_quantization_int8_constants(alpha=900 * alpha_Y, beta=900 * beta_Y)
-    Y_expected = torch.matmul(X, W)
-    Y_q_expected = quantization_int8(x=Y_expected, s=s_Y, z=z_Y)
+    # Compute Dequantized Matrix
+    Y_dq = dequantization(Y_q, s_Y, z_Y)
+    Y_dq_expected = dequantization(Y_q_expected, s_Y, z_Y)
 
-    Y_expected_prime = torch.matmul(X_q_dq, W_q_dq)
-    Y_expected_prime_q = quantization_int8(x=Y_expected_prime, s=s_Y, z=z_Y)
-    Y_expected_prime_q_dq = dequantization(x_q=Y_expected_prime_q,
-                                           s=s_Y,
-                                           z=z_Y)
+    # Store Results
+    params["Ground Truth Error"] = (torch.norm(Y - Y_dq.float()) / torch.norm(Y)).item()
+    params["Expected Error"] = (torch.norm(Y_dq_expected - Y_dq) / torch.norm(Y_dq_expected)).item()
+    params["Simulated Error"] = (torch.norm(Y_q_expected.float() - Y_q.float()) / torch.norm(Y_q_expected.float())).item()
 
-    print("Expected FP32 Y:")
-    print(Y_expected)
-    print("Expected FP32 Y Quantized:")
-    print(Y_q_expected)
-
-    Y_q_simulated = quantization_matrix_multiplication_int8(A_q=X_q,
-                                                            B_q=W_q,
-                                                            s_A=s_X,
-                                                            z_A=z_X,
-                                                            s_B=s_W,
-                                                            z_B=z_W,
-                                                            s_Y=s_Y,
-                                                            z_Y=z_Y)
-    Y_simulated = dequantization(x_q=Y_q_simulated, s=s_Y, z=z_Y)
-
-    print("Expected Quantized Y_q from Quantized Matrix Multiplication:")
-    print(Y_q_simulated)
-    print(
-        "Expected Quantized Y_q from Quantized Matrix Multiplication Dequantized:"
-    )
-    print(Y_simulated)
-
-    # Ensure the algorithm implementation is correct
-    rtol = 1e-5
-    assert torch.isclose(Y_simulated, Y_expected_prime_q_dq, rtol=rtol).all()
-    assert torch.isclose(Y_q_simulated, Y_expected_prime_q, rtol=rtol).all()
-    # assert (np.array_equal(Y_q_simulated, Y_expected_prime_q))
+    return params
 
 
 if __name__ == "__main__":
-    main()
+    client = mlflow.MlflowClient()
+
+    # Ensure MLFlow server is running, otherwise start it locally
+    tracking_uri = mlflow.get_tracking_uri()
+    if tracking_uri is None or tracking_uri == "":
+        mlflow.set_tracking_uri("http://127.0.0.1:8080")
+
+    # Create a new experiment
+    experiment_name = "gemm_matrix_size"
+
+    exp = client.search_experiments(filter_string="name='{}'".format(experiment_name))
+    if len(exp) == 0:
+        experiment_id = client.create_experiment(experiment_name)
+    else:
+        experiment_id = exp[0].experiment_id
+
+    # Run the experiment
+    for dist_type in ["uniform", "normal"]:
+        for cover in [0.7, 0.9, 0.95, 0.99]:
+            with mlflow.start_run(experiment_id=experiment_id, run_name=f"dist_type={dist_type}, cover={cover}") as run:
+                params = {"cover": 0.99, "dist_type": dist_type, "alpha_a": -1, "beta_a": 1, "alpha_b": -1, "beta_b": 1}
+                for n in [10, 100, 1000, 3000]:
+                    params = experiment(n, alpha_a=-1, beta_a=1, alpha_b=-1, beta_b=1, cover=cover, random=0,
+                                        dist_type=dist_type)
+                    expected_error = params["Expected Error"]
+                    simulated_error = params["Simulated Error"]
+                    ground_truth_error = params["Ground Truth Error"]
+
+                    # Log the results
+                    mlflow.log_metric("Expected Error", expected_error, step=n)
+                    mlflow.log_metric("Simulated Error", simulated_error, step=n)
+                    mlflow.log_metric("Ground Truth Error", ground_truth_error, step=n)
+
+            print(f"Experiment completed for dist_type={dist_type}, cover={cover}")
